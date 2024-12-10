f0209acf4ca421328252028ad9284ac481d826a5
Akshay Tolani <akshayshaam@gmail.com>
2024-12-09 21:14:34 -0500

    restart

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
 import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Home from './pages/home/index';
-import Register from './pages/register';
-import Login from './pages/login';
-import Profile from './users/Profile';
-import RecipeDetail from './pages/Recipe';
+import CollegeRecipeFinder from './CollegeRecipeFinder';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <HashRouter>
-        <Routes>
-          <Route path="/" element={<Navigate to="/CollegeRecipeFinder/Home" />} />
-          <Route path="/CollegeRecipeFinder/Home" element={<Home />} />
-          <Route path="/CollegeRecipeFinder/Login" element={<Login />} />
-          <Route path="/CollegeRecipeFinder/Register" element={<Register />} />
-          <Route path="/CollegeRecipeFinder/Profile" element={<Profile />} />
-          <Route path="/CollegeRecipeFinder/Recipes/:id/:title" element={<RecipeDetail />} />
-        </Routes>
-      </HashRouter>
+   <HashRouter>
+    <div>
+     <Routes>
+      <Route path="/" element={<Navigate to="Kanbas"/>}/>
+      <Route path="/CollegeRecipeFinder/*" element={<CollegeRecipeFinder />} />
+     </Routes>
     </div>
-  );
-}
-
-export default App;
+   </HashRouter>
+ );}
+ 
